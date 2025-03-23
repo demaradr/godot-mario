36eb25caecca14e44826f6e382d9b63fc1b3b9dc
ClaudioDYT <claudio.demartinez@gmail.com>
2025-03-22 23:21:42 -0600

    Claudio Update 3
    - Added Lighting
    - Added Background
    - Restricted Camera so we don't see outside of the 'boundary'
    - Added working Coyote time
    
    Notes:
    - Character feels too fast, and coyote time while feeling more satisfying has made him too op. Maybe slow character down?
    - Need title screen and end screen

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,15 +5,22 @@ const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var coyote_timer = $CoyoteTimer
+
+var can_coyote_jump = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() && (can_coyote_jump == false):
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor() || can_coyote_jump:
+			velocity.y = JUMP_VELOCITY
+			if can_coyote_jump:
+				can_coyote_jump = false
+				
 		
 	# Get the input direction and handle the movement/deceleration.
 	#Get the input direction: -1, 0, 1# As good practice, you should replace UI actions with custom gameplay actions.
@@ -31,5 +38,11 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-
+	var was_on_floor = is_on_floor()
 	move_and_slide()
+	if was_on_floor and !is_on_floor() and velocity.y >=0:
+		can_coyote_jump = true
+		coyote_timer.start()
+	
+func _on_coyote_timer_timeout() -> void:
+	can_coyote_jump = false
