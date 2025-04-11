f07d537179a0cd9fd40db06817373c8be19731f2
Adriano Demartin <adriano.demartinez@gmail.com>
2025-04-10 23:42:27 -0600

    added eyeball enemy
    added the logic behind the eyeball enemy. Still needs work with the sprites

--- Scripts/slime.gd
+++ Scripts/slime.gd
@@ -4,6 +4,7 @@ extends Node2D
 const SPEED = 30
 var direction = 1
 
+
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
@@ -17,5 +18,9 @@ func _process(delta: float):
 	if ray_cast_left.is_colliding():
 		direction = 1
 		animated_sprite_2d.flip_h = false
+		
+		
+		
+	animated_sprite_2d.play("idle")
 
 	position.x += direction * SPEED * delta
